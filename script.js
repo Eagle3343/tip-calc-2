136a6625f0c8586bdb77314ec0c3b16bd6656c80
Eagle3343 <Eagle3343>
2024-11-11 19:41:21 +0400

    code solved

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+function calcTip (bill, tip) {
+  let amount = bill + bill * tip
+
+  return amount
+}
+
+let bill = 275
+let tip = 0.15
+let amount = calcTip(bill, tip)
+
+if (bill >= 50 && bill <= 300) {
+  console.log(`the bill was ${bill}, the tip ${tip} ,the 
+       total is ${amount}`)
+} else if (bill >= 301) {
+  console.log(`the bill was ${bill}, the tip ${tip} ,the 
+       total is ${amount}`)
+}
